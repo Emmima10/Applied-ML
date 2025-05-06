a6e8cd23e622696aa8d81a9e5d6ad36464b1b14b
Emmima <60661915+Emmima10@users.noreply.github.com>
2025-05-06 10:16:49 -0500

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -2,14 +2,24 @@ from transformers import pipeline
 import streamlit as st
 
 # Step 1: Your knowledge base
-knowledge_base = """
-- The West region had the highest total sales.
-- Technology category was the most profitable.
-- Phones were the top-selling sub-category.
-- Discounts above 30% caused major profit reductions.
-- XGBoost model achieved RMSE: $39,000 and MAPE: 13.8%.
-- Random Forest model achieved RMSE: $42,000 and MAPE: 16.5%.
-"""
+with open("chatbotkb.txt", "r", encoding="utf-8") as f:
+    knowledge_base = f.read()
+#knowledge_base = 
+# """
+# - The West region had the highest total sales at $725,458.
+# - The most profitable product category was Technology.
+# - The top-selling sub-category by revenue was Phones.
+# - Discounts above 30% were associated with an average profit of $-107.21.
+# -Cluster 0.0: Sales: low , Profit: low , Discount: high , Quantity: high . 
+# This cluster has low engagement and is unprofitable. Focus on re-engaging these customers with promotions or incentives.
+# -Cluster 1.0: 
+# Sales: high , Profit: high , Discount: low , Quantity: high . 
+# This cluster represents high-value customers who generate substantial revenue and profit. Consider offering premium services or upselling opportunities.
+# -Cluster 2.0: 
+# Sales: low , Profit: low , Discount: low , Quantity: low . 
+# This cluster has low engagement and is unprofitable. Focus on re-engaging these customers with promotions or incentives
+
+# """
 
 # Step 2: Load a Question-Answering pipeline
 qa_pipeline = pipeline("text2text-generation", model="google/flan-t5-small")
@@ -25,6 +35,14 @@ def get_answer(user_input):
 def huggingface_qa_chatbot_terminal():
     print("🧠 Welcome to the FLAN-T5 Sales Insights Chatbot!")
     print("Ask me anything about sales, models, products. Type 'exit' to quit.")
+    # 💡 Suggested Questions
+    st.markdown("### 💡 Try asking:")
+    st.markdown("- What is Cluster 0?")
+    st.markdown("- Who are our best customers?")
+    st.markdown("- Which region has the highest sales?")
+    st.markdown("- How did Prophet perform?")
+    st.markdown("- Which product category is most profitable?")
+    st.markdown("- How many days are forecasted with sales below $10,000?")
     
     while True:
         user_input = input("\nYou: ")
